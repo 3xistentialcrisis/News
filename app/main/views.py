c9997bc537a66f33a7ebb03ffd60b37554225f76
Wanjiku Karanja <ciiku92@gmail.com>
2020-02-03 00:07:57 +0300

    Add def index function to view the application index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,16 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request, redirect, url_for
 from . import main
 from ..models import Sources
+from ..requests import get_news, get_sources
+
+@main.route('/')
+def index():
+    '''
+    This function returns the application index page
+    '''
+
+    title = 'Home || Welcome to your Daily News Aggregator'
+
+    
+    news = get_sources('source')
+    return render_template('index.html', title=title, sources= news)

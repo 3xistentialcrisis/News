6f5ea42296216d8d36822e9a9cf328802fcebb40
Wanjiku Karanja <ciiku92@gmail.com>
2020-02-02 21:26:07 +0300

    Create SourcesTest class & test if it is initialised

---
+++ tests/test_sources.py
@@ -0,0 +1,34 @@
+import unittest 
+from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of Sources Class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+
+        self.new_source = Sources('quartz', 'QuartzNews','Global news and business insights','business','usa','https://qz.com/')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+    
+    def test_init(self):
+        '''
+        Test if the Sources Class is initialised
+        '''
+
+        self.assertEqual(self.new_source.id,'quartz')
+        self.assertEqual(self.new_source.name,'QuartzNews')
+        self.assertEqual(self.new_source.description,'Global news and business insights')
+        self.assertEqual(self.new_source.category,'business')
+        self.assertEqual(self.new_source.country,'usa')
+        self.assertEqual(self.new_source.url,'https://qz.com/')
+
+
+if __name__ == '__main__':
+    unittest.main()
